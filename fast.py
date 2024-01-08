70a2e58bb020f1aa5756f5905e9e8e3e921ae03b
elida vellayoudom <vellayoudom.elida@gmail.com>
2024-01-08 15:03:03 +0100

    màj

--- fast.py
+++ fast.py
@@ -1,7 +1,7 @@
 # 1. Library imports
 import uvicorn
 from fastapi import FastAPI
-from Model import IrisModel, IrisSpecies
+from model import IrisModel, IrisSpecies
 
 # 2. Create app and model objects
 app = FastAPI()
